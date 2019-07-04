09651a6f53ef373fcd4426b06c0064aec0269165
Franco Marini <franco-marini@outlook.com>
2019-07-04 10:59:26 -0300

    LPPA2019: Board HTML structure by Javascript

--- app.js
+++ app.js
@@ -8,9 +8,24 @@ var initialState = [
     [ undefined, undefined, 1, 1, 1, undefined, undefined]
 ]
 
+//Declare varible and HTML structure
+var dynamicBoard = '<ul>'
 for (row = 0; row < initialState.length; row++) {
-    console.log('Row')
+    dynamicBoard += '<li>' 
     for (column = 0; column < initialState[row].length; column++) {
-        console.log(initialState[row][column])
+        if(initialState[row][column] == 1 || initialState[row][column] == 0 ){
+            dynamicBoard += '<button></button>'
+        }
     }
+    dynamicBoard += '</li>'
+}
+dynamicBoard += '</ul>'
+
+console.log(dynamicBoard)
+
+window.onload = function(){
+//Obteain the element 'board' on the HML
+const boardElement = document.getElementById('board')
+//Show HTML 
+boardElement.innerHTML = dynamicBoard;
 }
