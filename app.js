78c1d6ba440a4a18ce9a8bb6229eaec543c0a093
Franco Marini <franco-marini@outlook.com>
2019-07-04 10:27:17 -0300

    LPPA2019: Base files

---
+++ app.js
@@ -0,0 +1,9 @@
+var initialState = [
+    [ undefined, undefined,1,1,1, undefined, undefined],
+    [ undefined, undefined,1,1,1, undefined, undefined],
+    [ 1        ,         1,1,1,1,          1,        1],
+    [ 1        ,         1,1,0,1,          1,        1],
+    [ 1        ,         1,1,1,1,          1,        1],
+    [ undefined, undefined,1,1,1, undefined, undefined],
+    [ undefined, undefined,1,1,1, undefined, undefined]
+]
