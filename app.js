ec24b85987894916f352dec5c84f8e61b26c11bf
Franco Marini <franco-marini@outlook.com>
2019-07-04 10:43:27 -0300

    LPPA2019: Board values

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 var initialState = [
-    [ undefined, undefined,1,1,1, undefined, undefined],
-    [ undefined, undefined,1,1,1, undefined, undefined],
-    [ 1        ,         1,1,1,1,          1,        1],
-    [ 1        ,         1,1,0,1,          1,        1],
-    [ 1        ,         1,1,1,1,          1,        1],
-    [ undefined, undefined,1,1,1, undefined, undefined],
-    [ undefined, undefined,1,1,1, undefined, undefined]
+    [ undefined, undefined, 1, 1, 1, undefined, undefined],
+    [ undefined, undefined, 1, 1, 1, undefined, undefined],
+    [     1    ,     1    , 1, 1, 1,     1    ,     1    ],
+    [     1    ,     1    , 1, 0, 1,     1    ,     1    ],
+    [     1    ,     1    , 1, 1, 1,     1    ,     1    ],
+    [ undefined, undefined, 1, 1, 1, undefined, undefined],
+    [ undefined, undefined, 1, 1, 1, undefined, undefined]
 ]
+
+for (row = 0; row < initialState.length; row++) {
+    console.log('Row')
+    for (column = 0; column < initialState[row].length; column++) {
+        console.log(initialState[row][column])
+    }
+}
