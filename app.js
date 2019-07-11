2101d535217aff9ffa8e0cde5e77a2565721d43c
Franco Marini <franco-marini@outlook.com>
2019-07-11 11:23:38 -0300

    LPPA2019: Select peg and show suggestions

--- app.js
+++ app.js
@@ -1,37 +1,136 @@
-var initialState = [
-    [ undefined, undefined, 1, 1, 1, undefined, undefined],
-    [ undefined, undefined, 1, 1, 1, undefined, undefined],
-    [     1    ,     1    , 1, 1, 1,     1    ,     1    ],
-    [     1    ,     1    , 1, 0, 1,     1    ,     1    ],
-    [     1    ,     1    , 1, 1, 1,     1    ,     1    ],
-    [ undefined, undefined, 1, 1, 1, undefined, undefined],
-    [ undefined, undefined, 1, 1, 1, undefined, undefined]
+var board = [
+    [undefined, undefined, { value: 1 }, { value: 1 }, { value: 1 }, undefined, undefined],
+    [undefined, undefined, { value: 1 }, { value: 1 }, { value: 1 }, undefined, undefined],
+    [{ value: 1 }, { value: 1 }, { value: 1 }, { value: 1 }, { value: 1 }, { value: 1 }, { value: 1 }],
+    [{ value: 1 }, { value: 1 }, { value: 1 }, { value: 0 }, { value: 1 }, { value: 1 }, { value: 1 }],
+    [{ value: 1 }, { value: 1 }, { value: 1 }, { value: 1 }, { value: 1 }, { value: 1 }, { value: 1 }],
+    [undefined, undefined, { value: 1 }, { value: 1 }, { value: 1 }, undefined, undefined],
+    [undefined, undefined, { value: 1 }, { value: 1 }, { value: 1 }, undefined, undefined]
 ]
 
-//Declare varible and HTML structure
-var dynamicBoard = '<ul>'
-for (row = 0; row < initialState.length; row++) {
-    dynamicBoard += '<li>' 
-    for (column = 0; column < initialState[row].length; column++) {
-        if(initialState[row][column] == 1){
-            dynamicBoard += '<button class="ball-place"></button>'
-        }
-        else if (initialState[row][column] == 0){
-            dynamicBoard += '<button class="ball-place-empty"></button>'
-        }
-        else{
-            dynamicBoard += '<button class="empty-space" disabled></button>'
+var selectedPeg = { x: undefined, y: undefined }
+
+var createId = function (rowN, colN) {
+    return 'peg-' + rowN + '-' + colN
+}
+
+//Generate all de the cells in one row
+var generateCell = function (cell, rowN, colN) {
+    //Id is the location of the peg
+    var html = '<button id="' + createId(rowN, colN) + '" class="'
+    if (cell && cell.value) {
+        html += 'ballPlace'
+    }
+    else if (cell && cell.value == 0) {
+        html += 'ballPlaceEmpty'
+    }
+    else {
+        html += 'hidden'
+    }
+    html += '"></button>'
+    return html
+}
+
+//Generate the row
+var generateRow = function (row, rowN) {
+    var html = '<div class="row">'
+    for (let column = 0; column < row.length; column++) {
+        html += generateCell(row[column], rowN, column)
+    }
+    html += '</div>'
+    return html
+}
+
+//Generate all the rows for the game
+var generateBoard = function () {
+    var html = '<div class="row">'
+    for (row = 0; row < board.length; row++) {
+        html += generateRow(board[row], row)
+    }
+    html += '</div>'
+    return html
+}
+
+var unselectPeg = function () {
+    if (selectedPeg.x !== undefined && selectedPeg.y !== undefined) {
+        var prevSelectedId = createId(selectedPeg.x, selectedPeg.y)
+        document.getElementById(prevSelectedId).className = 'ballPlace'
+        var suggestions = document.getElementsByClassName('ballPlaceAvailable')
+        for (let i = 0; i < suggestions.length; i++) {
+            suggestions[i].className = 'ballPlaceEmpty'
         }
     }
-    dynamicBoard += '</li>'
 }
-dynamicBoard += '</ul>'
 
-console.log(dynamicBoard)
-
-window.onload = function(){
-//Obteain the element 'board' on the HML
-const boardElement = document.getElementById('board')
-//Show HTML 
-boardElement.innerHTML = dynamicBoard;
+var getElement = function(id){
+    var element = document.getElementById(id)
+    return element || {}
 }
+
+var showSuggestions = function () {
+    var near= {
+        above:  getElement(createId(selectedPeg.x-1,selectedPeg.y)),
+        left:   getElement(createId(selectedPeg.x,selectedPeg.y-1)),
+        right:  getElement(createId(selectedPeg.x,selectedPeg.y+1)),
+        below:  getElement(createId(selectedPeg.x+1,selectedPeg.y))
+    }
+
+    var possible= {
+        above:  getElement(createId(selectedPeg.x-2,selectedPeg.y)),
+        left:   getElement(createId(selectedPeg.x,selectedPeg.y-2)),
+        right:  getElement(createId(selectedPeg.x,selectedPeg.y+2)),
+        below:  getElement(createId(selectedPeg.x+2,selectedPeg.y))
+    }
+
+    if(near.above.className === 'ballPlace' && possible.above.className === 'ballPlaceEmpty'){
+        possible.above.className = 'ballPlaceAvailable'
+    }
+
+    if(near.left.className === 'ballPlace' && possible.left.className === 'ballPlaceEmpty'){
+        possible.left.className = 'ballPlaceAvailable'
+    }
+
+    if(near.right.className === 'ballPlace' && possible.right.className === 'ballPlaceEmpty'){
+        possible.right.className = 'ballPlaceAvailable'
+    }
+
+    if(near.below.className === 'ballPlace' && possible.below.className === 'ballPlaceEmpty'){
+        possible.below.className = 'ballPlaceAvailable'
+    }
+}
+
+var selectPeg = function (evt) {
+    var peg = evt.target
+    var idParts = peg.id && peg.id.length ? peg.id.split('-') : []
+    if (idParts.length === 3) {
+        unselectPeg()
+        if (selectedPeg.x === parseInt(idParts[1]) && selectedPeg.y === parseInt(idParts[2])) {
+            unselectPeg()
+            selectedPeg.x = undefined
+            selectedPeg.y = undefined
+        }
+        else {
+            selectedPeg.x = parseInt(idParts[1])
+            selectedPeg.y = parseInt(idParts[2])
+            peg.className = 'ballSelected'
+            showSuggestions()
+        }
+    }
+    console.log(selectedPeg)
+}
+
+var addPegsEventHandlers = function (pegs) {
+    for (let i = 0; i < pegs.length; i++) {
+        pegs[i].onclick = selectPeg
+    }
+}
+
+//Initialize the game
+var init = function () {
+    var boardElement = document.getElementById('board')
+    boardElement.innerHTML = generateBoard()
+    var pegs = boardElement.getElementsByClassName('ballPlace')
+    addPegsEventHandlers(pegs)
+}
+
+window.onload = init
