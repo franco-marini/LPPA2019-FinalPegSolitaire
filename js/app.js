74bf8e53dba2cdc68186e7bfac26654e6b4eb05b
Franco Marini <franco-marini@outlook.com>
2019-07-18 18:09:35 -0300

    LPPA2019: Tidy up the code

--- js/app.js
+++ js/app.js
@@ -8,80 +8,53 @@ var board = [
     [undefined, undefined, { value: 1 }, { value: 1 }, { value: 1 }, undefined, undefined]
 ]
 
-//#region Save a Game
-//Returns the date and hour of today
-function getDate() {
-    var date = new Date();
-    var yyyy = date.getFullYear();
-    var dd = date.getDate();
-    var mm = (date.getMonth() + 1);
 
-    //Puts the 0 for the numbers below 2 digits
-    if (dd < 10)
-        dd = "0" + dd;
-
-    if (mm < 10)
-        mm = "0" + mm;
-
-    var currentDay = yyyy + "-" + mm + "-" + dd;
-
-    var hours = date.getHours()
-    var minutes = date.getMinutes()
-    var seconds = date.getSeconds();
-
-    //Puts the 0 for the numbers below 2 digits
-    if (hours < 10)
-        hours = "0" + hours;
-
-    if (minutes < 10)
-        minutes = "0" + minutes;
-
-    if (seconds < 10)
-        seconds = "0" + seconds;
-
-    return currentDay + "-" + hours + ":" + minutes + ":" + seconds;
-}
-
-
-function compare( a, b ) {
-    //Order the saved games by date
-    if ( a.date < b.date ){
-      return -1;
-    }
-    if ( a.date > b.date ){
-      return 1;
-    }
-    return 0;
-}
-
-function saveGame() {
-
-    var nameTxt = document.getElementById('nameTxt').value
-
-    var newGame = {
-        date: getDate(),
-        name: nameTxt,
-        actualGame: board
-    }
-
-    document.getElementById('nameTxt').value = ''
-    //Save the game with de date value as the primary key
-    localStorage.setItem(getDate().toString(), JSON.stringify(newGame))
-    drawGamesTable()
-}
-
-function loadGame(){
-    board = findGame().actualGame
+var startGame = function(){
     var boardElement = document.getElementById('board')
     boardElement.innerHTML = generateBoard()
     var pegs = boardElement.getElementsByClassName('ballPlace')
     addPegsEventHandlers(pegs)
+    var holes = boardElement.getElementsByClassName('ballPlaceEmpty')
+    addHolesEventHandlers(holes)
+}
+
+//Returns the date and hour of today
+function getDate() {
+    var date = new Date()
+    var yyyy = date.getFullYear()
+    var dd = date.getDate()
+    var mm = (date.getMonth() + 1)
+    //Puts the 0 for the numbers below 2 digits
+    if (dd < 10) {
+        dd = "0" + dd
+    }
+    if (mm < 10) {
+        mm = "0" + mm
+    }
+    var currentDay = yyyy + "-" + mm + "-" + dd
+    var hours = date.getHours()
+    var minutes = date.getMinutes()
+    var seconds = date.getSeconds()
+    //Puts the 0 for the numbers below 2 digits
+    if (hours < 10) {
+        hours = "0" + hours
+    }
+    if (minutes < 10) {
+        minutes = "0" + minutes
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds
+    }
+    return currentDay + "T" + hours + ":" + minutes + ":" + seconds
+}
+
+function compare(a, b) {
+    //Order by descend the saved games by date
+    return new Date(b.date) - new Date(a.date);
 }
 
 function loadGames() {
-
     var storedGames = []
-
     if (storedGames == null) {
         storedGames = []
     }
@@ -89,26 +62,37 @@ function loadGames() {
         //Get all the items of localStorage, then save in a new variable the items parsing from JSON format
         for (var i = 0; i < localStorage.length; i++) {
             game = localStorage.getItem(localStorage.key(i))
+            //Converts/Parse JSON string into an object
             storedGames.push(JSON.parse(game))
-
         }
     }
     storedGames.sort(compare)
-    console.log(storedGames)
     return storedGames
 }
 
+function drawGamesTable() {
+    var list = loadGames()
+    var divList = document.getElementById('listGames')
+    //Draw with HTML structure a list of the saved games
+    divList.innerHTML = '<ul>'
+    for (let i = 0; i < list.length; i++) {
+        divList.innerHTML += '<li>' + list[i].date + " - " + list[i].name + ' <input type="radio" name="rbtGame" value="' + list[i].date + '">' + '</li>'
+    }
+    divList.innerHTML += '</ul>'
+}
+
 function findGame() {
+    //Brings all the saved games in localStorage
     var storedGames = loadGames()
-    var rates = document.getElementsByName('rbtGame');
-    var rateValue;
+    //Get all the elements with the name rbtGame (radio button)
+    var rates = document.getElementsByName('rbtGame')
+    var rateValue
     //Checks which radio button is select
     for (var i = 0; i < rates.length; i++) {
         if (rates[i].checked) {
-            rateValue = rates[i].value;
+            rateValue = rates[i].value
         }
     }
-
     //Compares the radio button selected with all the saved games
     var game
     for (let i = 0; i < storedGames.length; i++) {
@@ -117,8 +101,12 @@ function findGame() {
             game = storedGames[i]
         }
     }
-
     return game
+}
+
+function loadGame() {
+    board = findGame().actualGame
+    startGame()
 }
 
 function deleteGame() {
@@ -126,26 +114,42 @@ function deleteGame() {
     drawGamesTable()
 }
 
-function drawGamesTable() {
-    var list = loadGames()
-    var divList = document.getElementById('listGames')
-
-    //Draw with HTML structure a list of the saved games
-    divList.innerHTML = '<ul>'
-    for (let i = 0; i < list.length; i++) {
-        //Converts/Parse JSON string into an object
-        game = JSON.parse(localStorage.getItem(localStorage.key(i)))
-        divList.innerHTML += '<li>' + game.date + " - " + game.name + ' <input type="radio" name="rbtGame" value="' + game.date + '">' + '</li>'
+function saveGame() {
+    var nameTxt = document.getElementById('nameTxt').value
+    var newGame = {
+        date: getDate(),
+        name: nameTxt,
+        actualGame: board
     }
-    divList.innerHTML += '</ul>'
+    document.getElementById('nameTxt').value = ''
+    //Save the game with de date value as the primary key
+    localStorage.setItem(getDate().toString(), JSON.stringify(newGame))
+    drawGamesTable()
 }
-//#endregion
+
+var resetGame = function () {
+    location.reload()
+}
 
 //#region Functional of the game
 var selectedPeg = { x: undefined, y: undefined }
+var suggestions = []
 
+//Creates an id for a peg
 var createId = function (rowN, colN) {
     return 'peg-' + rowN + '-' + colN
+}
+
+//Get the position od the peg from an id
+var getPositionFromId = function (id) {
+    var idParts = id && id.length ? id.split('-') : []
+    if (idParts.length === 3) {
+        return {
+            x: parseInt(idParts[1]),
+            y: parseInt(idParts[2])
+        }
+    }
+    return {}
 }
 
 //Generate all de the cells in one row
@@ -218,22 +222,29 @@ var showSuggestions = function () {
 
     if (near.above.className === 'ballPlace' && possible.above.className === 'ballPlaceEmpty') {
         possible.above.className = 'ballPlaceAvailable'
+        suggestions.push(possible.above.id)
     }
 
     if (near.left.className === 'ballPlace' && possible.left.className === 'ballPlaceEmpty') {
         possible.left.className = 'ballPlaceAvailable'
+        suggestions.push(possible.left.id)
     }
 
     if (near.right.className === 'ballPlace' && possible.right.className === 'ballPlaceEmpty') {
         possible.right.className = 'ballPlaceAvailable'
+        suggestions.push(possible.right.id)
     }
 
     if (near.below.className === 'ballPlace' && possible.below.className === 'ballPlaceEmpty') {
         possible.below.className = 'ballPlaceAvailable'
+        suggestions.push(possible.below.id)
     }
+    console.log(suggestions)
 }
 
 var selectPeg = function (evt) {
+    //Clean the suggestions
+    suggestions = []
     var peg = evt.target
     var idParts = peg.id && peg.id.length ? peg.id.split('-') : []
     if (idParts.length === 3) {
@@ -250,7 +261,6 @@ var selectPeg = function (evt) {
             showSuggestions()
         }
     }
-    console.log(selectedPeg)
 }
 
 var addPegsEventHandlers = function (pegs) {
@@ -258,26 +268,50 @@ var addPegsEventHandlers = function (pegs) {
         pegs[i].onclick = selectPeg
     }
 }
+
+var movePeg = function (evt) {
+    var id = evt.target.id
+    var pos = getPositionFromId(id)
+    if (pos.x !== undefined && pos.y !== undefined) {
+        //Returns true if the element is in the array suggestions 
+        if (suggestions.includes(id)) {
+            var oldRow = selectedPeg.x
+            var oldCol = selectedPeg.y
+            var newRow = pos.x
+            var newCol = pos.y
+            var midRow = oldRow + ((newRow - oldRow) / 2)
+            var midCol = oldCol + ((newCol - oldCol) / 2)
+            board[oldRow][oldCol] = { value: 0 }
+            board[midRow][midCol] = { value: 0 }
+            board[newRow][newCol] = { value: 1 }
+            selectedPeg = { x: undefined, y: undefined }
+            suggestions = []
+            init()
+        }
+    }
+}
+
+var addHolesEventHandlers = function (holes) {
+    for (let i = 0; i < holes.length; i++) {
+        holes[i].onclick = movePeg
+    }
+}
 //#endregion
+
 
 //Initialize the game
 var init = function () {
     //localStorage.clear()
-    var boardElement = document.getElementById('board')
-    boardElement.innerHTML = generateBoard()
-    var pegs = boardElement.getElementsByClassName('ballPlace')
-    addPegsEventHandlers(pegs)
-
-
+    startGame()
     drawGamesTable()
     var btnSaveGame = document.getElementById('saveGame')
     btnSaveGame.onclick = saveGame
-
     var btnLoadGame = document.getElementById('loadGame')
     btnLoadGame.onclick = loadGame
-
     var btnDeleteGame = document.getElementById('deleteGame')
     btnDeleteGame.onclick = deleteGame
+    var btnResetGame = document.getElementById('resetGame')
+    btnResetGame.onclick = resetGame
 }
 
 window.onload = init
