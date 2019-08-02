0511b4c7248fef75ba882a033eea019cef4ced81
Franco Marini <franco-marini@outlook.com>
2019-08-02 13:14:08 -0300

    LPPA2019: Create file for functions for popup and menu

---
+++ js/popupMenu.js
@@ -0,0 +1,45 @@
+//Show the save menu
+//Change the style of the vertical menu
+var openNav = function() {
+    document.getElementById('verticalMenu').classList.add('width')
+    document.getElementById('content').classList.add('marginLeft')
+}
+
+//Hide the save menu
+//Change the style of the vertical menu
+var closeNav = function() {
+    document.getElementById('verticalMenu').classList.remove('width')
+    document.getElementById('content').classList.remove('marginLeft')
+}
+
+var closePopup = function() {
+    //Change by class the overlay to hidden
+    overlay = document.getElementsByClassName('overlay')[0]
+    overlay.classList.remove('active')
+    overlay.getElementsByTagName('form')[0].classList.add('inactive')
+}
+
+var showPopupMessage = function(message) {
+    var divMessage = document.getElementById('message')
+    divMessage.innerHTML = '<h2>' + message + '</h2>'
+}
+
+var openPopup = function(message) {
+    //Change by class the overlay to visible
+    overlay = document.getElementsByClassName('overlay')[0]
+    overlay.classList.add('active')
+    //Show the form to save the score on the high scores
+    overlay.getElementsByTagName('form')[0].classList.remove('inactive')
+    showPopupMessage(message)
+}
+
+var openPopupBtn = function() {
+    closeNav()
+    //Change by class the overlay to visible
+    overlay = document.getElementsByClassName('overlay')[0]
+    overlay.classList.add('active')
+    //Only show the High scores, it dosent show the form to save the score
+    overlay.getElementsByTagName('form')[0].classList.add('inactive')
+    var divMessage = document.getElementById('message')
+    divMessage.innerText = ''
+}
